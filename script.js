198d307c463e6b3e7e8e99cd091fbb7798467479
Kali Prasad Panda <80939822+kaliprasadp7@users.noreply.github.com>
2023-03-27 19:41:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let fc = new Promise((resolve, reject) => {
+	setTimeout(()=>{
+		resolve("Hello, world!");
+	},1000)
+	document.getElementById(output).innerHTML="Hello, world!";
+})
